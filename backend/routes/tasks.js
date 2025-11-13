b8d2e5d8193a3cfcbff5506ab72df94c80491bf4
Gabrielkaos <gabrielmontes155@gmail.com>
2025-11-13 21:53:09 +0800

    coded more in tasks

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,10 +1,33 @@
 const express = require("express")
 const db = require("../database")
+const authMiddleWare = require("../middleware/auth")
 
 router = express.Router()
-
+router.use(authMiddleWare)
 
 //api/tasks/
+
+router.post("/",(req, res)=>{
+    const {title, description, status} = req.body
+    const userId = req.user.userId
+
+    if(!title){
+        return res.status(401).json({error:"Title required"})
+    }
+
+    db.run(`INSERT INTO tasks (title, description, status, user_id) VALUES (?, ?, ?, ?)`,
+        [title, description || '', status || 'pending',userId],
+        (err)=>{
+            if(err){
+                return res.status(500).json({error:"Database Error"})
+            }
+            res.status(201).json({
+                message:"Task created successfully",
+                task:{id:this.lastID,title, description, status:status || 'pending',user_id:userId}
+            })
+        }
+    )
+})
 
 router.get("/:id",(req, res)=>{
     const taskId = req.params.id
